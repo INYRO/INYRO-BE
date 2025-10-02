8d32e18950af377a88ac9212cfad2e2492f5501f
Gyueon Lee <gon2yq@naver.com>
2025-10-03 03:38:45 +0900

    Init Project

---
+++ src/main/java/com/inyro/api/InyroApplication.java
@@ -0,0 +1,13 @@
+package com.inyro.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InyroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InyroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inyro/api/InyroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inyro.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InyroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
