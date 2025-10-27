218c371381cde9d8ef3aeb0e26d1f7141e393b13
Gyueon Lee <gon2yq@naver.com>
2025-10-27 20:48:02 +0900

    🚀chore: 자바 컨벤션

--- src/main/java/com/inyro/api/InyroApplication.java
+++ src/main/java/com/inyro/api/InyroApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InyroApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InyroApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InyroApplication.class, args);
+    }
 
 }
