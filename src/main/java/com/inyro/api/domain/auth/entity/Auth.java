d4b0ada6ce3c23fe5507d9445b9b994b86a31d5c
seohyunseok <tjgustjr33@gmail.com>
2025-10-28 00:26:12 +0900

    ✨ feat: auth 엔티티

--- src/main/java/com/inyro/api/domain/auth/entity/Auth.java
+++ src/main/java/com/inyro/api/domain/auth/entity/Auth.java
@@ -1,5 +1,6 @@
 package com.inyro.api.domain.auth.entity;
 
+import com.inyro.api.domain.member.entity.Member;
 import com.inyro.api.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
@@ -11,8 +12,21 @@ import lombok.*;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Table(name = "auth")
 public class Auth extends BaseEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "sno", nullable = false)
+    private String sno;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
 }
