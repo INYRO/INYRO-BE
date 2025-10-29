80d2475557b8c64461bed3892d95041c9334110a
seohyunseok <tjgustjr33@gmail.com>
2025-10-30 00:45:48 +0900

    ✨ feat: reservation 엔티티

--- src/main/java/com/inyro/api/domain/reservation/entity/Reservation.java
+++ src/main/java/com/inyro/api/domain/reservation/entity/Reservation.java
@@ -1,8 +1,12 @@
 package com.inyro.api.domain.reservation.entity;
 
+import com.inyro.api.domain.member.entity.Member;
 import com.inyro.api.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
 @Getter
@@ -15,4 +19,18 @@ public class Reservation extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String participantList;
+
+    private String purpose;
+
+    private LocalDate date;
+
+    private LocalTime startTime;
+
+    private LocalTime endTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
 }
