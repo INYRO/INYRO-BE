986d2ca6e39f953444001e1ce7619077b55c4029
seohyunseok <tjgustjr33@gmail.com>
2025-10-29 21:42:38 +0900

    ✨ feat: 회원 탈퇴 api

--- src/main/java/com/inyro/api/domain/member/service/command/MemberCommandService.java
+++ src/main/java/com/inyro/api/domain/member/service/command/MemberCommandService.java
@@ -4,4 +4,6 @@ import com.inyro.api.domain.member.entity.Member;
 
 public interface MemberCommandService {
     Member createMember(String name, String sno, String major);
+
+    void deleteMember(String Sno);
 }
