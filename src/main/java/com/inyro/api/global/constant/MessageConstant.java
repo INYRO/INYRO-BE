b6e55afded254f2d1fccf2e92bb007e545ff1cd4
Gyueon Lee <gon2yq@naver.com>
2025-10-30 17:29:02 +0900

    ✨feat: Auth 패키지 dto 검증 구현

---
+++ src/main/java/com/inyro/api/global/constant/MessageConstant.java
@@ -0,0 +1,14 @@
+package com.inyro.api.global.constant;
+
+public class MessageConstant {
+    public static final String WRONG_SNO = "학번은 숫자 9자리 입니다.";
+    public static final String WRONG_PASSWORD = "공백없는 4-16자리의 대/소문자+숫자+특수문자만 허용, 사용불가 특수문자 : <, >, {, }, |, ;, ', \"";
+
+    public static final String BLANK_SNO = "학번 누락";
+    public static final String BLANK_PASSWORD = "비밀번호 누락";
+    public static final String BLANK_NAME = "이름 누락";
+    public static final String BLANK_DEPT = "전공 누락";
+    public static final String BLANK_ENROLLED = "동아리 가입 여부 누락";
+    public static final String BLANK_NEW_PASSWORD = "새 비밀번호 누락";
+    public static final String BLANK_PASSWORD_CONFIRMATION = "비밀번호 확인 누락";
+}
