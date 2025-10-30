a615193d2af51bc1c1003d868f007665e0930a05
Gyueon Lee <gon2yq@naver.com>
2025-10-30 15:41:25 +0900

    ✨feat: 관리자 컨트롤러에 권한 설정 주석

--- src/main/java/com/inyro/api/domain/admin/controller/AdminController.java
+++ src/main/java/com/inyro/api/domain/admin/controller/AdminController.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,6 +26,7 @@ public class AdminController {
     private final AdminService adminService;
 
     @Operation(summary = "유저 목록 조회")
+//    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping()
     public CustomResponse<PageResponse<AdminResDto.MemberDetailResDto>> getAllUsers(
             @RequestParam(required = false) MemberSortType sortType,
